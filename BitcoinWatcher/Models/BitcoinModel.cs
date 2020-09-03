350e8d6633998f6580e89ad92e4d063033b99960
Matthew Almeida <matthew.almeida572@gmail.com>
2020-09-02 23:34:58 -0300

    Setting Model.

--- BitcoinWatcher/Models/BitcoinModel.cs
+++ BitcoinWatcher/Models/BitcoinModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,60 @@ using System.Threading.Tasks;
 
 namespace BitcoinWatcher.Models
 {
-    public class BitcoinModel
+    public partial class BitcoinModel
     {
-        public string Currency { get; set; }
-        public decimal Price { get; set; }
-        public DateTime ReturnTime { get; set; }
+        [JsonProperty("time")]
+        public Time Time { get; set; }
+
+        [JsonProperty("disclaimer")]
+        public string Disclaimer { get; set; }
+
+        [JsonProperty("chartName")]
+        public string ChartName { get; set; }
+
+        [JsonProperty("bpi")]
+        public Bpi Bpi { get; set; }
+    }
+
+    public partial class Bpi
+    {
+        [JsonProperty("USD")]
+        public Eur Usd { get; set; }
+
+        [JsonProperty("GBP")]
+        public Eur Gbp { get; set; }
+
+        [JsonProperty("EUR")]
+        public Eur Eur { get; set; }
+    }
+
+    public partial class Eur
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("rate")]
+        public string Rate { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("rate_float")]
+        public double RateFloat { get; set; }
+    }
+
+    public partial class Time
+    {
+        [JsonProperty("updated")]
+        public string Updated { get; set; }
+
+        [JsonProperty("updatedISO")]
+        public DateTimeOffset UpdatedIso { get; set; }
+
+        [JsonProperty("updateduk")]
+        public string Updateduk { get; set; }
     }
 }
