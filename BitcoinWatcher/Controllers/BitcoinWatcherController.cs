2cf67131c448c151a98024e42fd5eeb0eabeed0c
Matthew Almeida <matthew.almeida572@gmail.com>
2020-09-02 23:28:43 -0300

    Add project files.

---
+++ BitcoinWatcher/Controllers/BitcoinWatcherController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+using BitcoinWatcher.Models;
+using Newtonsoft.Json;
+
+namespace BitcoinWatcher.Controllers
+{
+    public class BitcoinWatcherController
+    {
+
+        public async Task<BitcoinModel> GetBitcoinPrice()
+        {
+            BitcoinModel Bitcoin = null;
+
+            try
+            {
+                var client = new HttpClient();
+
+                var response = await client.GetAsync($@"https://api.coindesk.com/v1/bpi/currentprice.json");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var Json = await response.Content.ReadAsStringAsync();
+
+                    Bitcoin = JsonConvert.DeserializeObject<BitcoinModel>(Json);
+                }
+
+                return Bitcoin;
+            }
+            catch (Exception)
+            {
+                return Bitcoin;
+            }
+
+        }
+    }
+}
---
+++ BitcoinWatcher/Models/BitcoinModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinWatcher.Models
+{
+    public class BitcoinModel
+    {
+        public string Currency { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReturnTime { get; set; }
+    }
+}
